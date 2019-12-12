812ec160e5b53b30d7ce5ef15cf2d301b87799ef
balbinamolerus <bmolerus@student.agh.edu.pl>
2019-12-12 11:42:14 +0100

    initial commits

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cakei on 2019-12-12.
+//
+
+#include "main.hpp"
---
+++ main.hpp
@@ -0,0 +1,8 @@
+//
+// Created by cakei on 2019-12-12.
+//
+
+#ifndef NETSIM_MAIN_HPP
+#define NETSIM_MAIN_HPP
+
+#endif //NETSIM_MAIN_HPP
