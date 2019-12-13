0e22a43594c7293dff344a7e19018d07f06ba577
Jakub Burczyk <jburczyk99@gmail.com>
2019-12-13 02:19:18 +0100

    ElementID alias Package class and its first methods

--- include/package.hpp
+++ include/package.hpp
@@ -1,4 +1,21 @@
 #ifndef NETSIM_PACKAGE_HPP
 #define NETSIM_PACKAGE_HPP
 
+#include <deque>
+
+#include "types.hpp"
+
+class Package{
+public:
+    Package();
+    Package(Package&& other);
+    Package& operator = (Package&& other);
+    ElementID get_id() const { return ID; };
+private:
+    ElementID ID;
+    static std::deque<ElementID> used_IDs;
+    bool is_ID_used(const ElementID &newID);
+    void assign_ID();
+};
+
 #endif //NETSIM_PACKAGE_HPP
--- src/package.cpp
+++ src/package.cpp
@@ -1,1 +1,33 @@
 #include "package.hpp"
+
+std::deque<ElementID> Package::used_IDs;
+
+Package::Package() {
+
+}
+
+Package::Package(Package &&other) {
+    other.get_id();
+}
+
+Package &Package::operator=(Package &&other) {
+    other.get_id();
+    return *this;
+}
+
+bool Package::is_ID_used(const ElementID &newID){
+    for(const auto& id: used_IDs){
+        if(newID == id){
+            return true;
+        }
+    }
+    return false;
+}
+
+void Package::assign_ID(){
+    ElementID newID = 1;
+    while(is_ID_used(newID)){
+        newID = newID + 1;
+    }
+    ID = newID;
+}
