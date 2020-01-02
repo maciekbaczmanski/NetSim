fad163cd823b8321ba2e42df912f6e7e98c97f14
maciekbaczmanski <cakeisalie16@gmail.com>
2020-01-02 13:11:35 +0100

    Nodes- added classes declarations

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -1,5 +1,64 @@
 #ifndef NETSIM_NODES_HPP
 #define NETSIM_NODES_HPP
+
+
+#include "helpers.hpp"
+#include "storage_types.hpp"
+#include <iostream>
+#include <optional>
+#include <memory>
+class IPackageReceiver
+{
+public:
+    void receive_package(Package&& p);
+    ElementID get_id();
+};
+
+class Storehouse
+{
+public:
+    Storehouse(std::unique_ptr<IPackageStockpile> d, ElementID id);
+};
+
+class ReceiverPreferences
+{
+public:
+    void add_receiver(IPackageReceiver* r);
+    void remove_receiver(IPackageReceiver* r);
+    IPackageReceiver* choose_receiver();
+};
+
+class PackageSender
+{
+public:
+    void send_package();
+    std::optional<Package> get_sending_buffer();
+    ReceiverPreferences receiver_preferences_;
+
+protected:
+    void push_package(Package&&);
+};
+
+class Ramp : public PackageSender
+{
+public:
+    Ramp(ElementID id, TimeOffset di);
+    void deliver_goods(Time t);
+    TimeOffset get_delivery_interval();
+    ElementID get_id();
+};
+
+class Worker : public PackageSender
+{
+public:
+    Worker(ElementID id, TimeOffset pd, std::unique_ptr<PackageQueue> q);
+    void do_work(Time t);
+    TimeOffset get_processing_duration();
+    Time get_package_processing_start_time();
+};
+
+
+
 //TODO:  classes: IPackageReceiver, Storehouse, ReceiverPreferences, PackageSender, Ramp i Worker
 
 /*
