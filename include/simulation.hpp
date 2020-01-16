2e2485e2d6bb034813a668fae40698a492f80bd9
maciekbaczmanski <cakeisalie16@gmail.com>
2020-01-16 13:34:50 +0100

    Factory check

---
+++ include/simulation.hpp
@@ -0,0 +1,11 @@
+#include "factory.hpp"
+#include "nodes.hpp"
+#include "types.hpp"
+
+#ifndef NETSIM_SIMULATION_HPP
+#define NETSIM_SIMULATION_HPP
+
+void simulate(Factory  &factory, TimeOffset d,std::function<void(Factory&,Time)> func) {});
+
+
+#endif //NETSIM_SIMULATION_HPP
---
+++ src/simulation.cpp
@@ -0,0 +1,11 @@
+#include "simulation.hpp"
+#include "types.hpp"
+#include "factory.hpp"
+
+void simulate(Factory &factory, TimeOffset d,std::function<void(Factory&,Time)> func) {})
+{
+    for(TimeOffset i=1;i<=d;i++)
+        {
+
+        }
+}
